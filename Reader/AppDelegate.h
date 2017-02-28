862ab1f7e8f2a1fbee44d519f5e6926e7899ba2b
Jujuxw <13389634266@189.cn>
2017-02-28 18:33:26 +0800

    Initial Commit

---
+++ Reader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reader
+//
+//  Created by juju on 2017/2/28.
+//  Copyright © 2017年 juju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Reader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Reader
+//
+//  Created by juju on 2017/2/28.
+//  Copyright © 2017年 juju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
